439144355a725a93496d96442df0f1a6a767e7af
da1234 <darrell.adjei@hotmail.com>
2017-09-27 23:02:36 +0100

    Update MainThread.java

--- app/src/main/java/com/example/DA/rebound/MainThread.java
+++ app/src/main/java/com/example/DA/rebound/MainThread.java
@@ -10,24 +10,18 @@ import android.view.SurfaceHolder;
 public class MainThread extends Thread {
 
     public static  final String TAG= MainActivity.class.getSimpleName();
-
-
     private int FPS= 50;
     private SurfaceHolder mSurfaceHolder;
     private MainActivity.Game mGame;
     private  boolean running;
     public  static Canvas mCanvas;
     boolean paused=false;
-
-
     public MainThread(SurfaceHolder surfaceHolder, MainActivity.Game game){
         super();
         mSurfaceHolder = surfaceHolder;
         mGame = game;
-
     }
-
-
+    
     @Override
     public  void run(){
         long startTime;
@@ -36,56 +30,39 @@ public class MainThread extends Thread {
         long totalTime =0;
         int frameCount=0;
         long targetTime=1000/FPS;
-
-
         while(running){
             startTime = System.nanoTime();
             mCanvas=null;
-
             //try locking the Canvas for pixel editing
-
             try{
                 mCanvas = mSurfaceHolder.lockCanvas();
-
                 synchronized (mSurfaceHolder){
                     //Log.v(TAG,"flopping");
                     mGame.update();
                     mGame.Draw(mCanvas);
                 }
-
-
-
             }catch (Exception e){
                 e.printStackTrace();
                 Log.v(TAG, "flopped");
             }
             while(running && paused){
                 mGame.Draw(mCanvas);
-
                 try {
                     this.sleep(50);
                 }catch (InterruptedException e){
                     Log.v(TAG,"peak");
                 }
-
-
             }
-
             timeMillis = (System.nanoTime()-startTime)/1000000;
             waitTime= targetTime-timeMillis;
-
             try{
-
                 this.sleep(waitTime);
-
             }catch (Exception e){
                 Log.v(TAG,"also flopped");
             }
-
             finally {
                 if(mCanvas!=null){
                     try{
-
                         mSurfaceHolder.unlockCanvasAndPost(mCanvas);
                     }catch (Exception e){
                         e.printStackTrace();
@@ -96,23 +73,19 @@ public class MainThread extends Thread {
             frameCount++;
             if(frameCount==30){
                 frameCount=0;
-
             }
         }
     }
+    
     public void setRunning(boolean b){
-
         this.running=b;
-
-
     }
+    
     public void setPaused(boolean b){
         paused=b;
-
     }
 
     public  boolean getPaused(){
-
         return paused;
     }
 }
